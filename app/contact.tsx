5d7ac5b96326500006cbe8df152c11dc18627f9b
likhita <likhita.balaga@webileapps.com>
2026-07-24 12:54:31 +0530

    contact us page implementation

--- app/contact.tsx
+++ app/contact.tsx
@@ -1,12 +1,98 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Appearance, Linking } from "react-native";
 import React from "react";
+import { Colors } from "@/constants/theme";
+import { Fontisto } from "@expo/vector-icons";
 
 const contact = () => {
+  const colorScheme = Appearance.getColorScheme();
+  const theme = colorScheme === "dark" ? Colors.dark : Colors.light;
+  const imgColor = colorScheme === "dark" ? "papayawhip" : "#333";
+  const styles = createStyles(theme, colorScheme);
   return (
-    <View>
-      <Text>contact</Text>
+    <View style={styles.screen}>
+      <View style={styles.imgContainer}>
+        <Fontisto name="coffeescript" size={250} color={imgColor} />
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.title}>Coffee Shop</Text>
+        <Text style={styles.body}>
+          555 Coffee Lane{"\n"}Kansas City, KS 55555-1234
+        </Text>
+
+        <Text style={styles.label}>Phone:</Text>
+        <Text
+          style={styles.link}
+          onPress={() => Linking.openURL("tel:5555555555")}
+        >
+          555-555-5555
+        </Text>
+        <Text
+          style={styles.link}
+          onPress={() => Linking.openURL("sms:5555555555")}
+        >
+          or Click Here to Text!
+        </Text>
+
+        <Text style={styles.label}>Hours:</Text>
+        <Text style={styles.body}>Open 6am to 4pm daily.</Text>
+      </View>
     </View>
   );
 };
 
 export default contact;
+function createStyles(
+  theme: {
+    text: string;
+    background: string;
+    headerbackground: string;
+    tint: string;
+    icon: string;
+    tabIconDefault: string;
+    tabIconSelected: string;
+  },
+  colorScheme: string | null | undefined,
+) {
+  return StyleSheet.create({
+    screen: {
+      flex: 1,
+      backgroundColor: theme.background,
+    },
+    topBand: {
+      height: 20,
+      backgroundColor: theme.headerbackground,
+    },
+    content: {
+      paddingHorizontal: 12,
+      paddingTop: 12,
+    },
+    title: {
+      color: theme.text,
+      fontSize: 26,
+      fontWeight: "bold",
+      lineHeight: 31,
+      marginBottom: 10,
+    },
+    body: {
+      color: theme.text,
+      fontSize: 18,
+      lineHeight: 27,
+    },
+    label: {
+      color: theme.text,
+      fontSize: 18,
+      lineHeight: 27,
+      marginTop: 12,
+    },
+    link: {
+      color: theme.text,
+      fontSize: 18,
+      lineHeight: 27,
+      textDecorationLine: "underline",
+    },
+    imgContainer: {
+      backgroundColor: colorScheme === "dark" ? "#353636" : "#D0D0D0",
+      height: 250,
+    },
+  });
+}
--- app/menu.tsx
+++ app/menu.tsx
@@ -18,18 +18,32 @@ const menu = () => {
   const theme = colorScheme === "dark" ? Colors.dark : Colors.light;
   const styles = createStyles(theme, colorScheme);
   const Container = Platform.OS === "web" ? ScrollView : SafeAreaView;
+  const separatorComp = () => <View style={styles.separator} />;
+  // const headerComp = () => <Text>Top of the List</Text>;
+  const footerComp = () => (
+    <Text style={{ color: theme.text }}>End of the List</Text>
+  );
   return (
     <Container>
       <FlatList
         data={MENU_ITEMS}
         keyExtractor={(item) => item.id.toString()}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.contentContainer}
+        ItemSeparatorComponent={separatorComp}
+        // ListHeaderComponent={headerComp}
+        ListFooterComponent={footerComp}
+        ListFooterComponentStyle={styles.footerStyle}
+        ListEmptyComponent={<Text>No items</Text>}
         renderItem={({ item }) => (
-          <View>
-            <View>
-              <Text>{item.title}</Text>
-              <Text>{item.description}</Text>
+          <View style={styles.row}>
+            <View style={styles.MenuTextRow}>
+              <Text style={[styles.menuItemTitle, styles.menuItemText]}>
+                {item.title}
+              </Text>
+              <Text style={styles.menuItemText}>{item.description}</Text>
             </View>
-            <Image source={MENU_IMAGES[item.id - 1]} />
+            <Image style={styles.menuImage} source={MENU_IMAGES[item.id - 1]} />
           </View>
         )}
       />
@@ -51,5 +65,54 @@ function createStyles(
   },
   colorScheme: string | null | undefined,
 ) {
-  return StyleSheet.create({});
+  return StyleSheet.create({
+    contentContainer: {
+      paddingTop: 10,
+      paddingBottom: 20,
+      paddingHorizontal: 12,
+      backgroundColor: theme.background,
+    },
+    separator: {
+      height: 1,
+      backgroundColor: colorScheme === "dark" ? "papayawhip" : "#000",
+      width: "50%",
+      maxWidth: 300,
+      marginHorizontal: "auto",
+      marginBottom: 10,
+    },
+    footerStyle: {
+      marginHorizontal: "auto",
+    },
+    row: {
+      flexDirection: "row",
+      width: "100%",
+      maxWidth: 600,
+      height: 100,
+      marginBottom: 10,
+      borderStyle: "solid",
+      borderColor: colorScheme === "dark" ? "papayawhip" : "#000",
+      borderWidth: 1,
+      borderRadius: 20,
+      overflow: "hidden",
+      marginHorizontal: "auto",
+    },
+    MenuTextRow: {
+      width: "65%",
+      paddingTop: 10,
+      paddingLeft: 10,
+      paddingRight: 5,
+      flexGrow: 1,
+    },
+    menuItemTitle: {
+      fontSize: 18,
+      textDecorationLine: "underline",
+    },
+    menuItemText: {
+      color: theme.text,
+    },
+    menuImage: {
+      width: 100,
+      height: 100,
+    },
+  });
 }
